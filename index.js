057e957ba97d115e605ad3e7cd3340ca3df62527
dungcv2 <dungcv2@vng.com.vn>
2021-11-04 16:32:59 +0700

    add proxy

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require("express");
+// // const proxy = require("express-http-proxy");
+const path = require("path");
+const PORT = process.env.PORT || 8000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  // .get("/", (req, res) => res.render("pages/index"))
+  .use(
+    "/",
+    createProxyMiddleware({
+      target: "https://speech.vinbase.ai",
+      changeOrigin: true,
+    })
+  )
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// app.listen(3000);
+
+// express()
+// .use(express.static(path.join(__dirname, "public")))
+// .set("views", path.join(__dirname, "views"))
+// .set("view engine", "ejs")
+// .get("/", (req, res) => res.render("pages/index"))
+//   .use(proxy("/api", { target: "https://speech.vinbase.ai" }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
